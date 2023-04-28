1cc37f5fb433c2df91e44dccc779d2b9bcd683eb
muha <1student.muhammed1@gmail.com>
2023-04-29 01:35:25 +0300

    format the code

--- 1. Two Sum/script.js
+++ 1. Two Sum/script.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 
-function twoSum(nums, target) {
+function twoSum(nums, target) 
+{
     const map = new Map(); //create a new map
-    for (let i = 0; i < nums.length; i++) { // ^ loop through the array 
-
+    for (let i = 0; i < nums.length; i++) // ^ loop through the array 
+    { 
         // cur + x = target 
-        // x = target - cur 
+        // x = target - cur  
         const x = target - nums[i];
-        if (map.has(x)) { // ~ map.has(x) it means is the target in the map or not in the map
 
+        if (map.has(x))  // ~ map.has(x) it means is the target in the map or not in the map
+        {
             return [map.get(x), i]; // ~ getting the value from the key (x)
         }
         map.set(nums[i], i);// ! adding (key , value) 
