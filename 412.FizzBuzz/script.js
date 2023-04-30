41234816732a26d1c6c3eca6de51698b002516da
muha <126491139+nmcev@users.noreply.github.com>
2023-04-30 23:17:31 +0300

    added comments

--- 412.FizzBuzz/script.js
+++ 412.FizzBuzz/script.js
@@ -4,13 +4,13 @@
  */
 var fizzBuzz = function(n) 
 {
-  let result = [];
+  let result = []; //array to hold the result 
     
     for(let i = 1;i <= n;i++)
     {
-      if( i % 3 == 0 && i % 5  == 0 )
+      if( i % 3 == 0 && i % 5  == 0 ) 
       {
-        result.push("FizzBuzz")
+        result.push("FizzBuzz") //pushing "FizzBuzz" to the array(result) if The IF statement true
       }else if (i % 5  == 0)
       {
         result.push("Buzz");
@@ -20,7 +20,7 @@ var fizzBuzz = function(n)
         result.push("Fizz");
       }
       else{
-        result.push(i.toString())   
+        result.push(i.toString())// turn the intger into string ===> 1 => '1'
        }
     }
 
