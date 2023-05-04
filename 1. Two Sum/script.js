5d7b9e30287895c6f3e56e8430b32a09b49ce907
muha <126491139+nmcev@users.noreply.github.com>
2023-05-04 18:04:31 +0300

    if statement to ternary method in Two sum

--- 1. Two Sum/script.js
+++ 1. Two Sum/script.js
@@ -13,10 +13,10 @@ function twoSum(nums, target)
         // x = target - cur  
         const x = target - nums[i];
 
-        if (map.has(x))  // ~ map.has(x) it means is the target in the map or not in the map
-        {
-            return [map.get(x), i]; // ~ getting the value from the key (x)
-        }
+        map.has(x)? return [map.get(x), i]; 
+        // ~ map.has(x) it means is the target in the map or not in the map
+             // ~ getting the value from the key (x)
+  
         map.set(nums[i], i);// ! adding (key , value) 
     }
     return null;
