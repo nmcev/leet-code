a9593ff5fa5525d66ebaf12b32f52907dd349a6a
muha <1student.muhammed1@gmail.com>
2023-12-25 12:00:12 +0300

    feat: add 'Product of Array Except Self' to Array & Hashing directory

---
+++ neetcode-roadmap/Array & Hashing/ProductofArrayExceptSelf.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+
+    const ans = [];
+    const leftProduct = Array(nums.length).fill(1);
+    const rightProduct = Array(nums.length).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+    }
+
+    for (let i = nums.length - 2; i >= 0; i--) {
+        rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(leftProduct[i] * rightProduct[i]);
+    }
+
+    return ans;
+
+};
