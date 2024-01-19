288c0262015eb622e28f85e3ec606c594a35c8f0
muha <1student.muhammed1@gmail.com>
2024-01-19 20:54:54 +0300

    refactor: reduce the if statements of 'ValidParentheses' with an object

--- neetcode-roadmap/stack/ValidParentheses.js
+++ neetcode-roadmap/stack/ValidParentheses.js
@@ -3,25 +3,28 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    const stack = [];
-  
-    for (let i = 0; i < s.length; i++) {
-  
-      const char = s[i];
-      if (char === '(') {
-        stack.push(')') 
-      } else if (char === '{') {
-  
-        stack.push('}') 
-  
-      }else if (char === '[') {
-  
-        stack.push(']') 
-  
-      } else if (stack.pop()!== char) {
-        return false
-      }
-    }
-    return stack.length === 0
-  
+  const stack = [];
+
+  const dictionary = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
   }
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i]
+
+    if (char in dictionary) {
+      stack.push(dictionary[char])
+    } else {
+
+      const top = stack.pop();
+      if (top !== char) {
+        return false;
+      }
+
+    }
+  }
+
+  return stack.length === 0
+};
