f3a1a7f8d0a9099beba43f3b88d39081f70bdf42
muha <1student.muhammed1@gmail.com>
2024-03-05 18:51:34 +0300

    feat: happy number(202) problem added

---
+++ 202.HappyNumber/happyNumber.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def calculate(n):
+            return sum([int(i) ** 2 for i in str(n)])
+        
+        def isHappyRecursive(n, seen):
+         if n == 1:
+            return True
+         
+         if n < 0 or n in seen:
+            return False 
+
+         seen.add(n) 
+         sum_of_squares = calculate(n)
+         return isHappyRecursive(sum_of_squares, seen)
+    
+        seen = set()
+        return isHappyRecursive(n, seen)
+        
+
+
+# Time complexity: O(logn)
+# Space complexity: O(logn)
+
+sol = Solution()
+happy = 2
+print(sol.isHappy(happy))  # Output: False
